d6fd341b7e97438b875e55a3cc574e3fff32924b
usfstd <usf.stdd@gmail.com>
2021-08-10 18:10:28 +0430

    add expation utility to each todo, and improvments in 'done!' icon

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,27 +1,45 @@
-import TodoItemExteraElements from "./TodoItemExteraElements";
-
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 import RadioButtonCheckedIcon from "@material-ui/icons/RadioButtonChecked";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import ListItem from "@material-ui/core/ListItem";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import { Divider } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-import { useState } from "react";
+import TodoItemExteraElements from "./TodoItemExteraElements";
+import TodoList from "./TodoList";
+
+import { useState, useContext } from "react";
+import { TodosContext } from "./todos-context";
+
 const styles = (theme) => ({
   main: {
+    width: "100%",
     margin: theme.spacing(0),
+    paddingLeft: "0",
+    paddingBottom: "0",
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
     justifyContent: "space-between",
-    "& #todoTitle": {
+
+    "& .singleItem": {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+    },
+    "& .todoTitle": {
       display: "flex",
       width: "100%",
       justifyContent: "space-between",
     },
-    "& #todoDescription span": {
+    "& .todoDescription span": {
       paddingRight: "2rem",
       fontSize: "0.7rem",
     },
@@ -31,35 +49,67 @@ const styles = (theme) => ({
   },
 });
 
-function TodoItem({ todo, classes }) {
-  const [hover, toggleHover] = useState(true);
+function TodoItem({ todo, todoHasChild, classes }) {
+  const [hover, toggleHover] = useState(false);
+  const [expaned, toggleExpaned] = useState(false);
+  const [isCompleted, toggleIsCompleted] = useState(todo.isCompleted);
+
+  const { isCompletedTodo } = useContext(TodosContext);
 
   return (
-    <ListItem
-      className={classes.main}
-      onMouseEnter={() => toggleHover(true)}
-      onMouseLeave={() => toggleHover(false)}
-    >
-      <div id="todoTitle">
-        <FormControlLabel
-          control={
-            <Checkbox
-              icon={<RadioButtonUncheckedIcon />}
-              checkedIcon={<RadioButtonCheckedIcon />}
-            />
-          }
-          label={<Typography>{todo.task}</Typography>}
-        />
+    <ListItem className={classes.main}>
+      <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
+        <div
+          className="singleItem"
+          onMouseEnter={() => toggleHover(true)}
+          onMouseLeave={() => toggleHover(false)}
+        >
+          <div className="todoTitle">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <IconButton
+                style={{
+                  padding: "0px",
+                  visibility: todoHasChild ? "visible" : "hidden",
+                }}
+                onClick={() => toggleExpaned(!expaned)}
+              >
+                {!expaned && <ChevronLeftIcon />}
+                {expaned && <ExpandMoreIcon />}
+              </IconButton>
+              <Checkbox
+                checked={isCompleted}
+                onClick={() => {
+                  isCompletedTodo(todo.id);
+                  toggleIsCompleted(!isCompleted);
+                }}
+                icon={<RadioButtonUncheckedIcon />}
+                checkedIcon={<CheckCircleOutlineIcon />}
+              />
+              {<Typography>{todo.task}</Typography>}
+            </div>
 
-        <div className={classes.extera}>
-          {hover && <TodoItemExteraElements />}
+            <div className={classes.extera}>
+              {hover && <TodoItemExteraElements />}
+            </div>
+          </div>
+          <div className="todoDescription">
+            <Typography>
+              <span>{todo.description}</span>
+            </Typography>
+          </div>
+        </div>
+        <div style={{ paddingRight: "31px" }}>
+          <Divider />
         </div>
       </div>
-      <div id="todoDescription">
-        <Typography>
-          <span>{todo.description}</span>
-        </Typography>
-      </div>
+
+      {expaned && <TodoList parentId={todo.id} />}
     </ListItem>
   );
 }
