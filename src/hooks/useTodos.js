1b825017afd553de2d20c4e92ee91ca866447137
usfstd <usf.stdd@gmail.com>
2021-08-10 23:56:39 +0430

    isCompleted function added

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,19 +1,35 @@
-import { useState, React } from "react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 function useTodos(initialVal) {
-  const [todos, setTodo] = useState(initialVal);
+  const [todos, setTodos] = useState(initialVal);
   const addTodo = (task, description) => {
-    setTodo([
+    setTodos([
       ...todos,
       {
-        id: 5,
+        id: uuidv4(),
         task: task,
         description: description,
-        isComplited: false,
+        isCompleted: false,
         parentId: null,
       },
     ]);
   };
+  const isCompletedTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) return { ...todo, isCompleted: !todo.isCompleted };
+        return todo;
+      })
+    );
+  };
 
-  return [todos, addTodo, setTodo];
+  // const todoHasChild = (id) => {
+  //   for (let i = 0; i < todos.length; i++) {
+  //     if (todos[i].parentId === id) return true;
+  //   }
+  //   return false;
+  // };
+  return [todos, addTodo, isCompletedTodo, setTodos];
 }
 export default useTodos;
